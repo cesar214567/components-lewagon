d4ad99f8f79f99dae58b2a203e5054b9abe51060
cesar madera <camgcamg11@gmail.com>
2021-08-24 18:53:34 -0500

    hole bunch of games added

---
+++ js/juegos.js
@@ -0,0 +1,30 @@
+var juegos = ['megaman8',
+              'megamanX',
+              'megamanX2',
+              'megamanX3',
+              'megamanX4',
+              'megamanX5',
+              'megamanX6',
+              'gdarius',
+              'metalslugX',
+              'bloodyroar',
+              'bloodyroar2',
+              'digimonrumblearena']
+
+const cargarJuegos = () =>{
+  headers = $("#headers");
+  banners = $("#banners");
+  juegos.forEach( juego =>{
+
+
+    headers.append(`<li><a href="${juego}.html">${juego}</a></li>`)
+    banners.append(`<div class=" ${juego} banner" >
+            <div class="container">
+                <h1>${juego} </h1>
+                <p>Juego del milenio : u </p>
+                <a href="${juego}.html" class="btn-new"> Ir a la pagina</a>
+            </div>
+        </div>`)
+  });
+
+}
