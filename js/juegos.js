e83a77859a480aac0de291c436df477d32d40f9e
cesar214567 <39888085+cesar214567@users.noreply.github.com>
2026-02-13 20:24:57 -0600

    Update juegos.js to easy adding games

--- js/juegos.js
+++ js/juegos.js
@@ -1,30 +1,60 @@
-var juegos = ['megaman8',
-              'megamanX',
-              'megamanX2',
-              'megamanX3',
-              'megamanX4',
-              'megamanX5',
-              'megamanX6',
-              'gdarius',
-              'metalslugX',
-              'bloodyroar',
-              'bloodyroar2',
-              'digimonrumblearena']
+// Mapa: nombre del juego -> URL de retrogames
+const juegosMap = {
+  megaman8: "https://www.retrogames.cc/embed/40233-mega-man-8.html",
+  megamanX: "https://www.retrogames.cc/embed/40191-mega-man-x.html",
+  megamanX2: "https://www.retrogames.cc/embed/40192-mega-man-x2.html",
+  megamanX3: "https://www.retrogames.cc/embed/40193-mega-man-x3.html",
+  megamanX4: "https://www.retrogames.cc/embed/40194-mega-man-x4.html",
+  megamanX5: "https://www.retrogames.cc/embed/40195-mega-man-x5.html",
+  megamanX6: "https://www.retrogames.cc/embed/40196-mega-man-x6.html",
+  gdarius: "https://www.retrogames.cc/embed/41603-g-darius.html",
+  metalslugX: "https://www.retrogames.cc/embed/40285-metal-slug-x.html",
+  bloodyroar: "https://www.retrogames.cc/embed/41622-bloody-roar.html",
+  bloodyroar2: "https://www.retrogames.cc/embed/41623-bloody-roar-2.html",
+  digimonrumblearena: "https://www.retrogames.cc/embed/41660-digimon-rumble-arena.html"
+};
 
-const cargarJuegos = () =>{
-  headers = $("#headers");
-  banners = $("#banners");
-  juegos.forEach( juego =>{
+// Cargar headers y dejar banners vacío
+const cargarJuegos = () => {
+  const headers = $("#headers");
+  const banners = $("#banners");
 
+  headers.html("");
+  banners.html(""); // vacío al cargar la página
 
-    headers.append(`<li class="btn-headers"><a href="${juego}.html" >${juego}</a></li>`)
-    banners.append(`<div class=" ${juego} banner" >
-            <div class="container">
-                <h1>${juego} </h1>
-                <p>Juego del milenio : u </p>
-                <a href="${juego}.html" class="btn-new"> Ir a la pagina</a>
-            </div>
-        </div>`)
+  Object.keys(juegosMap).forEach(juego => {
+    headers.append(`
+      <li class="btn-headers">
+        <a href="#" data-juego="${juego}">${juego}</a>
+      </li>
+    `);
   });
+};
 
-}
+// Evento click para cargar el juego
+$(document).on("click", ".btn-headers a", function (e) {
+  e.preventDefault();
+
+  const juego = $(this).data("juego");
+  const url = juegosMap[juego];
+
+  $("#banners").html(`
+    <div class="centered">
+      <iframe
+        src="${url}"
+        width="960"
+        height="720"
+        frameborder="no"
+        allowfullscreen
+        webkitallowfullscreen
+        mozallowfullscreen
+        scrolling="no">
+      </iframe>
+    </div>
+  `);
+});
+
+// Inicialización
+$(document).ready(() => {
+  cargarJuegos();
+});
